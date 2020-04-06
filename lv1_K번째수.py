a69e020bcb1871d87b6d6943d3810acd5ac62295
brucekwak <777kwak@gmail.com>
2020-04-06 13:47:16 +0900

    lv1_K번째수

---
+++ lv1_K번째수.py
@@ -0,0 +1,14 @@
+# [풀이 #1] for문 활용
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i, j, k = command
+        target_num = sorted(array[(i-1):j])[k-1]
+        answer.append(target_num)
+
+    return answer
+
+# [풀이 #2] map 활용
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
